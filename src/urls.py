68fc070b97f501578a6ed264869be37d8428a422
Charlie Redpath <github@chorl.dev>
2023-07-24 19:25:42 +0100

    Initial move from AWS

---
+++ src/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for site project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path, include
+from .views import basic
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path("", basic.index, name="index"),
+    path("contact", basic.contact, name="contact"),
+    path("about", basic.about, name="about"),
+    path("about-site", basic.site, name="about-site"),
+    path("credits", basic.credits, name="credits"),
+    path("projects/", include("src.projects.urls")),
+    path("robots.txt", basic.robots, name="robots")
+]
---
+++ src/views/basic.py
@@ -0,0 +1,44 @@
+from random import shuffle
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def index(request: HttpRequest):
+    return render(request, "index.html")
+
+
+def contact(request: HttpRequest):
+    return render(request, "contact.html")
+
+
+def about(request: HttpRequest):
+    label_list = [
+        ["Queer", "An umbrella term for being part of the LGBTQIA+ community."],
+        ["Autistic", "A person with <a href=\"https://autism.org.uk/advice-and-guidance/what-is-autism\" target=\"_blank\">Autism Spectrum Disorder</a>."],
+        ["Socialist", "A general term for views regarding the collective ownership of capital by the people or the state, and other associated social policy; the polar opposite of capitalism."],
+        ["Anarchist", "A term relating to the urgent need for violent upheaval of the current system &ndash; societal and political."],
+        ["Trans", "[Generally] someone who's gender assigned at birth does not align with their gender identity."],
+        ["Non-binary", "Someone who's gender identity does not fit the typical binary of male/female."],
+        ["Gender Fluid", "Someone who's gender identity is not constant."],
+        ["Republican (UK)", "Someone who does not agree with the notion of a monarch/monarchy."],
+        ["Environmentalist", "Someone who advocates for the protection of the environment."],
+        ["Activist", "Someone who partakes in activism."]
+    ]
+    shuffle(label_list)
+    return render(request, "about.html", context={"label_list": label_list})
+
+
+def site(request: HttpRequest):
+    return render(request, "about-site.html")
+
+
+def credits(request: HttpRequest):
+    return render(request, "credits.html")
+
+
+def robots(request: HttpRequest):
+    return HttpResponse(
+        "user-agent: *\n"
+        "disallow: all",
+        request
+    )
