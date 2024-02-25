314f48d996d63a1f7d234ab7c9856ce8d8c0bb5e
Charlie Redpath <github@chorl.dev>
2024-02-25 13:20:38 +0000

    Change anarchist definition

--- src/views/basic.py
+++ src/views/basic.py
@@ -16,7 +16,7 @@ def about(request: HttpRequest):
         ["Queer", "An umbrella term for being part of the LGBTQIA+ community."],
         ["Autistic", "A person with <a href=\"https://autism.org.uk/advice-and-guidance/what-is-autism\" target=\"_blank\">Autism Spectrum Disorder</a>."],
         ["Socialist", "A general term for views regarding the collective ownership of capital by the people or the state, and other associated social policy; the polar opposite of capitalism."],
-        ["Anarchist", "A term relating to the urgent need for violent upheaval of the current system &ndash; societal and political."],
+        ["Anarchist", "Similar to how Chomsy defines anarchism: the anti-authoritarian wing of socialism."],
         ["Trans", "[Generally] someone who's gender assigned at birth does not align with their gender identity."],
         ["Non-binary", "Someone who's gender identity does not fit the typical binary of male/female."],
         ["Gender Fluid", "Someone who's gender identity is not constant."],
