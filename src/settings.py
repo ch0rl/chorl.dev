d15db7c0d4f3dc2705e4255d4a22b4f3fcdeabbd
Autumn R <github@chorl.dev>
2024-06-07 14:58:00 +0100

    fix: undo db changes for now

--- src/settings.py
+++ src/settings.py
@@ -76,13 +76,17 @@ DB_NAME, DB_USER, DB_PASS, DB_HOST = (os.environ.get('DB_NAME', 'chorl-dev'), os
                                       os.environ.get("DB_PASS", "postgres"), os.environ.get("DB_HOST", "localhost"))
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': DB_NAME,
+    #     'USER': DB_USER,
+    #     'PASSWORD': DB_PASS,
+    #     'HOST': DB_HOST,
+    #     'OPTIONS': {'sslmode': 'require'},
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': DB_NAME,
-        'USER': DB_USER,
-        'PASSWORD': DB_PASS,
-        'HOST': DB_HOST,
-        'OPTIONS': {'sslmode': 'require'},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
