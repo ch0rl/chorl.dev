058e31961fd601ae144bca1a77c5414488fddcef
Autumn R <github@chorl.dev>
2024-07-01 22:28:46 +0100

    major: add new "terminology" project

--- src/projects/urls.py
+++ src/projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import index, kinks
+from .views import index, kinks, terminology
 
 urlpatterns = [
     path("", index, name="projects-index"),
-    path("kinks", kinks, name="kinks")
+    path("kinks", kinks, name="kinks"),
+    path("terms", terminology, name="terms"),
 ]
--- src/projects/views.py
+++ src/projects/views.py
@@ -8,3 +8,39 @@ def index(request: HttpRequest):
 
 def kinks(request: HttpRequest):
     return render(request, "projects/kinks.html")
+
+
+def terminology(request: HttpRequest):
+    return render(request, "projects/terms.html", context={"terms": [
+        ('Abort, Terminate', 'Cancel, Stop, End, Finalise', ''),
+        ('Normal', '', ''),
+        ('Blacklist', 'Denylist, Blocklist', ''),
+        ('Whitelist', 'Allowlist, Approvedlist', ''),
+        ('Blackhat', 'Hostile actor, Hostile force', ''),
+        ('Whitehat', 'Friendly actor, Security tester, ethical hacker', ''),
+        ('Greyhat', '', ''),
+        ('Black box', 'Opaque box, Zero-knowledge', ''),
+        ('White box', 'Clear box, Open box', ''),
+        ('Blind', 'Anonymous, Zero-knowledge', ''),
+        ('Double blind', 'Double anonymous', ''),
+        ('Dark', 'Deceptive, Manipulative', ''),
+        ('Motherboard', 'Primary board, Main board', ''),
+        ('Daughter board', 'Secondary board, Auxiliary board', ''),
+        ('Female connector', 'Socket, Concave connector', ''),
+        ('Male connector', 'Plug, Convex connector', ''),
+        ('Master', 'Main, Primary, Leader, Host', ''),
+        ('Slave', 'Replica, Secondary, Follower, Client', ''),
+        ('Orphan', 'Unreferenced, Unlinked, De-referenced', ''),
+        ('Sanity check', 'Confidence check, Unit test, Quick check', ''),
+        ('Back door', '', ''),
+        ('Red team', 'Outside testers, Attacking team, Attack simulators', ''),
+        ('Blue team', 'Defensive team', ''),
+        ('Purple team', '', ''),
+        ('Demilitarised Zone', 'Public service network, Insecure segment', ''),
+        ('Hacker (negative)', 'Threat actor, Attacker', ''),
+        ('Man-in-the-middle', 'Interception, Interceptor, Person-in-the-middle', ''),
+        ('Salt', '', ''),
+        ('Trojan horse', '', ''),
+        ('Zero day', '', ''),
+    ]})
+
