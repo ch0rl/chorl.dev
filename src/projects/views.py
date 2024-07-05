b48f00389a9405dbb3e291a9d55e36d2230167f5
Autumn R <github@chorl.dev>
2024-07-05 13:27:44 +0100

    feat(terms): move to CSV instead of hard-coded list

--- src/projects/views.py
+++ src/projects/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.conf import settings
 
 
 def index(request: HttpRequest):
@@ -10,37 +11,30 @@ def kinks(request: HttpRequest):
     return render(request, "projects/kinks.html")
 
 
+def get_terms():
+    # Only read file once
+    # TODO: When updated to use DB, will have to read each time for live updates
+    with open(settings.BASE_DIR / "src/projects/terms.csv") as f:
+        __raw_terms = f.read().splitlines()[1:]
+
+    terms = []
+    for line in __raw_terms:
+        parts = [i.strip() for i in line.split("|")]
+        if len(parts) == 3:
+            terms.append(parts)
+        else:
+            print(f"[!] failed parsing '{line}' into three '|'-seperated parts.")
+
+    return terms
+
+
+if settings.PROD:
+    TERMS = get_terms()
+
+
 def terminology(request: HttpRequest):
-    return render(request, "projects/terms.html", context={"terms": [
-        ('Abort, Terminate', 'Cancel, Stop, End, Finalise', 'Abort and terminate carry violent connotations.'),
-        ('Normal', '', ''),
-        ('Blacklist', 'Denylist, Blocklist', ''),
-        ('Whitelist', 'Allowlist, Approvedlist', ''),
-        ('Blackhat', 'Hostile actor, Hostile force', ''),
-        ('Whitehat', 'Friendly actor, Security tester, ethical hacker', ''),
-        ('Greyhat', '', ''),
-        ('Black box', 'Opaque box, Zero-knowledge', ''),
-        ('White box', 'Clear box, Open box', ''),
-        ('Blind', 'Anonymous, Zero-knowledge', ''),
-        ('Double blind', 'Double anonymous', ''),
-        ('Dark', 'Deceptive, Manipulative', ''),
-        ('Motherboard', 'Primary board, Main board', ''),
-        ('Daughter board', 'Secondary board, Auxiliary board', ''),
-        ('Female connector', 'Socket, Concave connector', ''),
-        ('Male connector', 'Plug, Convex connector', ''),
-        ('Master', 'Main, Primary, Leader, Host', ''),
-        ('Slave', 'Replica, Secondary, Follower, Client', ''),
-        ('Orphan', 'Unreferenced, Unlinked, De-referenced', ''),
-        ('Sanity check', 'Confidence check, Unit test, Quick check', ''),
-        ('Back door', '', ''),
-        ('Red team', 'Outside testers, Attacking team, Attack simulators', ''),
-        ('Blue team', 'Defensive team', ''),
-        ('Purple team', '', ''),
-        ('Demilitarised Zone', 'Public service network, Insecure segment', ''),
-        ('Hacker (negative)', 'Threat actor, Attacker', ''),
-        ('Man-in-the-middle', 'Interception, Interceptor, Person-in-the-middle', ''),
-        ('Salt', '', ''),
-        ('Trojan horse', '', ''),
-        ('Zero day', '', ''),
-    ]})
+    if settings.DEBUG:
+        return render(request, "projects/terms.html", context={"terms": get_terms()})
+
+    return render(request, "projects/terms.html", context={"terms": TERMS})
 
