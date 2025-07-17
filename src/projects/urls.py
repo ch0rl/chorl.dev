b5e3321b710c251cfd9d582907a85adff69c7535
Autumn R <github@chorl.dev>
2025-07-17 16:36:08 +0100

    feat(kinks): actually remove kinks for now

--- src/projects/urls.py
+++ src/projects/urls.py
@@ -3,7 +3,7 @@ from .views import index, kinks, terminology, browser, terms_api
 
 urlpatterns = [
     path("", index, name="projects-index"),
-    path("kinks", kinks, name="kinks"),
+    # path("kinks", kinks, name="kinks"),
     path("terms", terminology, name="terms"),
     path("browser", browser, name="browser"),
     path("api/terms", terms_api, name="api-terms"),
