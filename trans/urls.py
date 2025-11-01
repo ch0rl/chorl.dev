2e3e2f9e96acdf357217842305dc05c43149d867
Autumn R <github@chorl.dev>
2025-11-01 19:47:45 +0000

    fix(trans)

--- trans/urls.py
+++ trans/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from trans.views import *
 
 urlpatterns = [
-    path('', index, name='trans-index'),
+    path('', index, name='trans'),
     path('/email/<str:template>', email, name='email'),
     path('/category/<str:template>', category, name='category')
 ]
